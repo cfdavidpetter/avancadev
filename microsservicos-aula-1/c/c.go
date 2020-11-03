d8614a662681b51d9a86ef646ffe4d2082794e33
David Petter <cfdavidpetter@gmail.com>
2020-11-03 16:54:06 -0300

    #desafio-1

--- microsservicos-aula-1/c/c.go
+++ microsservicos-aula-1/c/c.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 type Coupon struct {
@@ -45,7 +47,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 	coupon := r.PostFormValue("coupon")
 	valid := coupons.Check(coupon)
 
+	resultCoupon := makeHttpCall("http://localhost:9093", coupon)
+
 	result := Result{Status: valid}
+
+	if resultCoupon.Status == "invalid" {
+		result.Status = "invalid"
+	}
 
 	jsonResult, err := json.Marshal(result)
 	if err != nil {
@@ -53,5 +61,31 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, string(jsonResult))
+
+}
+
+func makeHttpCall(urlMicroservice string, coupon string) Result {
+
+	values := url.Values{}
+	values.Add("coupon", coupon)
+
+	res, err := http.PostForm(urlMicroservice, values)
+	if err != nil {
+		result := Result{Status: "Servidor fora do ar!"}
+		return result
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error processing result")
+	}
+
+	result := Result{}
+
+	json.Unmarshal(data, &result)
+
+	return result
 
 }
---
+++ microsservicos-aula-1/d/d.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Result struct {
+	Status string
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":9093", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	coupon := r.PostFormValue("coupon")
+
+	result := Result{Status: "invalid"}
+
+	if coupon == "abc" {
+		result.Status = "approved"
+	}
+
+	jsonData, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal("Error processing json")
+	}
+
+	fmt.Fprintf(w, string(jsonData))
+}
